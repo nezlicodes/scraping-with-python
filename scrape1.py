3ea3668ffaaa49b866c11a3d3cea88fd1fb16f5c
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-09-12 10:28:01 +0200

    bs4

--- scrape1.py
+++ scrape1.py
@@ -1,13 +1,19 @@
 import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
 
 
-fhand = urllib.request.urlopen("https://www.kherdja.com/")
 
-counts= dict()
-for line in fhand:
-#    Line is in UTF-8 so we need to decode it first
-    words = line.decode().split()
-    for word in words:
-        counts[word] = counts.get(word, 0) + 1
+url = input(" Enter - ")
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
 
-print(counts)
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+
+#counts= dict()
+#for line in html:
+##    Line is in UTF-8 so we need to decode it first
+#    words = line.decode().split()
+#    for word in words:
+#        counts[word] = counts.get(word, 0) + 1
