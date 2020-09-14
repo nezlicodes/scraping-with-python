de9e5344f786727e92f8080e7973423343b8fab3
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-09-14 10:42:14 +0200

    extract quot author and tags

--- quotetutorial/quotetutorial/spiders/quote_spider.py
+++ quotetutorial/quotetutorial/spiders/quote_spider.py
@@ -9,3 +9,7 @@ class QuoteSpider(scrapy.Spider):
     
     def parse(self, response):
         all_divs = response.css('div.quote')
+        title = all_divs.css("span.text::text").extract()
+        author =all_divs.css(".author::text").extract()
+        tags = all_divs.css(".tag::text").extract()
+        yield {'title': title, 'author': author, 'tags': tags }
