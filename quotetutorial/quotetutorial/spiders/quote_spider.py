50d1e570387858458e6db1dc7cae7368019c7da4
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-09-14 18:14:54 +0200

    json works/

--- quotetutorial/quotetutorial/spiders/quote_spider.py
+++ quotetutorial/quotetutorial/spiders/quote_spider.py
@@ -19,3 +19,7 @@ class QuoteSpider(scrapy.Spider):
             items['author'] = author
             items['tags'] = tags
             yield items
+            
+        next_url = response.css("li.next a::attr(href)").get()
+        if next_url is not None:
+            yield response.follow(next_url, callback = self.parse)
